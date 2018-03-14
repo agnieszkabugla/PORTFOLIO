8d3853bd3a90e8fd6711dce8f704ad37798ffde7
agnieszkabugla <agnieszka.bugla3@gmail.com>
2018-03-14 19:57:18 +0100

    added image slider SLICK

--- source/index.js
+++ source/index.js
@@ -1,5 +1,30 @@
 import jQuery from 'jquery'; 
 
-jQuery(document).ready(function($){
-
+$(document).ready(function(){
+    $('.myslider').slick({
+        autoplay: true,
+        dots: true,
+        arrows: false,
+        cssEase: 'ease-out',
+        speed: 1000,
+        autoplaySpeed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
 }); 
