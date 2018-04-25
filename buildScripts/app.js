952937787ab74deec918a6177312f23131db6195
agnieszkabugla <agnieszka.bugla3@gmail.com>
2018-04-25 10:12:52 +0200

    added send mail functions

--- buildScripts/app.js
+++ buildScripts/app.js
@@ -1,35 +1,109 @@
-import express from 'express'; 
-import path from 'path'; 
+import express from 'express';
+import path from 'path';
 import webpack from 'webpack';
-import config from '../webpack.config.dev'; 
+import config from '../webpack.config.dev';
+import fs from 'fs';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
 
 /* eslint-disable no-console */
 
 
-const port = 8080; 
+const port = 8080;
 const app = express();
 
 //WEBPACK CONFIG
-const compiler = webpack(config); 
+const compiler = webpack(config);
 app.use(require('webpack-dev-middleware') (compiler, {
     noInfo: true,
     publickPath: config.output.publicPath
-})); 
+}));
 
 
 app.use(express.static(path.join(__dirname, '../source')));
 
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, '../public/index.html'))
-//     // __dirname -> The directory name of the current module.
-// })
 
-// /public/styles/css/styles.css
+//function saving a newly signed email to the file emails.csv
+function saveEmail(dataToWrite) {
+  fs.appendFile('emails.csv', `${dataToWrite.email},${dataToWrite.name},${dataToWrite.message}\n`, (err) => {
+  if (err) {
+    console.log('Some error occured - file either not saved or corrupted file saved.');
+  } else {
+    console.log('The new email has been successfully appended to emails.csv!');
+  }
+});
+}
+
+function sendMail(formData) {
+  //console.log(`login used to send email: ${process.env.USER}`);
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.USER,// generated ethereal user
+      pass: process.env.PASSWORD// generated ethereal password
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: process.env.FROMEMAIL, // sender address
+    to: process.env.MAINEMAIL, // list of receivers
+    subject: 'A new mail from PORTFOLIO page', // Subject line
+    text: `You have got one new message from: \n
+      ${formData.name} \n
+      ${formData.email} \n
+      MESSAGE:  \n
+      ${formData.message}`
+  };
+
+  // setup email data to the nw userwith unicode symbols
+  let mailOptions2 = {
+    from: process.env.FROMEMAIL, // sender address
+    to: formData.email, // list of receivers
+    subject: 'Thank you for your message', // Subject line
+    text: ' Thanks! ',
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
+
+  transporter.sendMail(mailOptions2, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  });
+}
+
+app.use(bodyParser.json());
+
+app.post('/sendMail', (req, res) => {
+  console.log(req.body);
+  sendMail(req.body);
+  saveEmail(req.body);
+  res.send({ success: "true" });
+});
+
+
 
 app.listen(port, function (err) {
-    if(err) {
-        console.log(err); 
-    } else {
-        console.log('server is running, PORT ' + port);
-    }   
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('server is running, PORT ' + port);
+  }
 });
--- source/index.js
+++ source/index.js
@@ -2,30 +2,73 @@
 import jQuery from 'jquery';
 
 $(document).ready(function(){
-    $('.myslider').slick({
-        autoplay: true,
-        dots: true,
-        arrows: false,
-        cssEase: 'ease-out',
-        speed: 1000,
-        autoplaySpeed: 1000,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
+
+  $("button").click(function() {
+    $("input").each(function() {
+      if($(this).val() != '' && $("textarea").val() != '') {
+        var email = $("#mail").val();
+        var name = $("#name").val();
+        var message = $("#msg").val();
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (filter.test(email) == true) {
+          var dataToSend = {
+            email: email,
+            name: name,
+            message: message
+          };
+          $.ajax({
+            type: "POST",
+            url: "/sendMail",
+            // The key needs to match your method's input parameter (case-sensitive).
+            data: JSON.stringify(dataToSend),
+            contentType: "application/json",
+            dataType: "json"
+          })
+          .done(function() {
+            alert("success");
+          })
+          .fail(function(data) {
+            alert("error");
+          })
+          .always(function() {
+            alert("completed");
+          });
+        } else {
+          alert("wrong email address");
+        }
+      } else {
+        alert("not all fields are filled")
+      }
     });
+  });
+
+
+
+
+  $('.myslider').slick({
+    autoplay: true,
+    dots: true,
+    arrows: false,
+    cssEase: 'ease-out',
+    speed: 1000,
+    autoplaySpeed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
